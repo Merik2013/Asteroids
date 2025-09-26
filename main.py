6a02c0fbd2203090455d1931b67732c7d9a3eb9d
Merik2013 <merik2013@gmail.com>
2025-09-26 00:57:05 -0400

    Player shape added

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     #dt is "delta time"
     while True:
@@ -14,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         #limit the FPS to 60
